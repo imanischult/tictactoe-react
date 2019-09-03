e259874811205e0cce3d19df7cba5cfe85e04bba
Imani Schulterbrandt <imani.vicente@outlook.com>
2019-09-03 00:45:14 -0400

    adding state to parent board component. function onClick not yet defined

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={function() {
-          alert("click");
+        onClick={() => {
+          this.props.onClick();
         }}
       >
         {this.props.value}
@@ -18,8 +18,20 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
